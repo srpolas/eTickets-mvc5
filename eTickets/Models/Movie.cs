e6863481c63fce3201cb6d19f37697542c9e9d58
srpolas <srpolas.bd@gmail.com>
2024-10-19 00:07:47 +0600

    Models, DbContext, ConnectionString CreatedAdd project files.

---
+++ eTickets/Models/Movie.cs
@@ -0,0 +1,35 @@
+﻿using eTickets.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+
+        //Relationships
+        public List<Actor_Movie> Actors_Movies { get; set; }
+
+        //Cinema
+        public int CinemaId { get; set; }
+        [ForeignKey("CinemaId")]
+        public Cinema Cinema { get; set; }
+
+        //Producer
+        public int ProducerId { get; set; }
+        [ForeignKey("ProducerId")]
+        public Producer Producer { get; set; }
+
+
+
+    }
+}
