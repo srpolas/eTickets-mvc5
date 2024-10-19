be61e6557e48b1af9bbcb80ad49c083692d063f3
srpolas <srpolas.bd@gmail.com>
2024-10-20 01:01:40 +0600

    Controller and Views Added !

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -3,17 +3,20 @@ using System.ComponentModel.DataAnnotations;
 
 namespace eTickets.Models
 {
-    public class Actor
-    {
-        [Key]
-        public int Id { get; set; }
-        public string ProfilePictureURL { get; set; }
-        public string FullName { get; set; }
-        public string Bio { get; set; }
+	public class Actor
+	{
+		[Key]
+		public int Id { get; set; }
+		[Display(Name = "Profile Picture")]
+		public string ProfilePictureURL { get; set; }
+		[Display(Name = "Full Name")]
+		public string FullName { get; set; }
+		[Display(Name = "Biography")]
+		public string Bio { get; set; }
 
-        //Relationships
-        public List<Actor_Movie> Actors_Movies { get; set; }
+		//Relationships
+		public List<Actor_Movie> Actors_Movies { get; set; }
 
 
-    }
+	}
 }
