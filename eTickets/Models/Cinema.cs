8d6f201c7e2e248b25e4c02794bf2b8ae57810f6
srpolas <srpolas.bd@gmail.com>
2024-10-21 01:40:52 +0600

    Cemena And Movies View Added

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -3,24 +3,28 @@ using System.ComponentModel.DataAnnotations;
 
 namespace eTickets.Models
 {
-    public class Cinema
-    {
-        [Key]
-        public int Id { get; set; }
+	public class Cinema
+	{
+		[Key]
+		public int Id { get; set; }
+		[Display(Name = "Cinema Logo")]
+		public string Logo { get; set; }
 
-        public string Logo { get; set; }
+		[Display(Name = "Cinema Name")]
 
-        public string Name { get; set; }
+		public string Name { get; set; }
 
-        public string Description { get; set; }
+		[Display(Name = "Description")]
 
-        //Relationships
-        public List<Movie> Movies { get; set; }
+		public string Description { get; set; }
 
-
-
-
+		//Relationships
+		public List<Movie> Movies { get; set; }
 
 
-    }
+
+
+
+
+	}
 }
